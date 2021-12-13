bf3501e232dd757b1030185e16c32647b8089edf
StVu1991 <vukelic.stjepan@gmail.com>
2021-12-13 17:53:07 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Ovo će biti cool aplikacija!
+          Ovo će biti cool aplikacija!!!
         </a>
       </header>
     </div>
